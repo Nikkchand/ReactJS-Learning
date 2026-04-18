1f3ac4737715b10f5ef18e07171ddf356cf35b4d
Rahul Chand <rahulchandrc188@gmail.com>
2026-04-18 20:06:05 +0530

    01-Folder done

---
+++ 01-Folder/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div id= 'parent'>
+      <h1 id = 'child1'>App.jsx</h1>
+      <h3 id='child2'>Hello from RC , I am learning ReactJS</h3>
+    </div>
+    <div id='parent2'>Hey bro!!</div>
+    </>
+  )
+}
+
+export default App
